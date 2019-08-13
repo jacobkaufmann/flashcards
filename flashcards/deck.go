a740337fcb68574b3886508467111a4a14c702b1
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2019-08-12 20:52:19 -0700

    Add basic deck methods.

--- flashcards/deck.go
+++ flashcards/deck.go
@@ -1,7 +1,41 @@
 package flashcards
 
+import (
+	"math/rand"
+	"time"
+)
+
 // Deck represents a deck of cards.
 type Deck struct {
 	cards []*Card
 	index int
+}
+
+// NewDeck returns a deck containing cards.
+func NewDeck(cards []*Card) *Deck {
+	return &Deck{
+		cards: cards,
+	}
+}
+
+// Draw returns the card in the deck at position index.
+func (d *Deck) Draw() *Card {
+	c := d.cards[d.index]
+
+	d.index++
+	if d.index == len(d.cards) {
+		d.index = 0
+	}
+
+	return c
+}
+
+// Shuffle rearranges the cards in a pseudo-random manner and resets the index
+// to zero.
+func (d *Deck) Shuffle() {
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(d.cards), func(i, j int) {
+		d.cards[i], d.cards[j] = d.cards[j], d.cards[i]
+	})
+	d.index = 0
 }
